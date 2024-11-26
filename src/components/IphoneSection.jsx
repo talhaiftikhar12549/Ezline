43717b04710c8c01da8963b633c71f18b3e5a62e
talhaiftikhar12549 <talhaiftikhar12549@gmail.com>
2024-11-27 00:25:14 +0500

    iphone seection

--- src/components/IphoneSection.jsx
+++ src/components/IphoneSection.jsx
@@ -1,13 +1,23 @@
 import React from "react"
-
+import iphonePic from "../assets/eziline-device-1365x2048.png"
 export default function IphoneSection()
 {
     return(
         <>
         <div className="h-[100vh] bg-gradient-to-b from-[#838995] to-[#4e7282] ">
 
-            <div className="">
-
+            <div className="flex h-[100%] w-[100%] py-12">
+                <div className="w-[40%]">
+                    <div>website development</div>
+                    <div>web development</div>
+                    <div>Mobile app development</div>
+                </div>
+                <div className="w-[20%] py-8"><img src={iphonePic} width="100%" height="100%" /></div>
+                <div className="w-[40%]">
+                <div>website development</div>
+                    <div>web development</div>
+                    <div>Mobile app development</div>
+                </div>
             </div>
         </div>
         </>
