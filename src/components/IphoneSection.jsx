b10f875aefca028a6177f2e196504f70550adfe3
talhaiftikhar12549 <talhaiftikhar12549@gmail.com>
2024-12-03 01:47:59 +0500

    iphone section compeleted

--- src/components/IphoneSection.jsx
+++ src/components/IphoneSection.jsx
@@ -9,19 +9,56 @@ export default function IphoneSection() {
                     <div className="w-[20%]">
 
 
-                        <div className="text-right"><div className="text-[#ffffff] text-3xl font-semibold"><h2 className="hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out">WEBSITE DEVELOPMENT</h2><p className="text-base">Developing a website is fun and anyone can do this but very few understand how to build websites</p></div>
-                            <div className="text-[#ffffff] text-3xl font-semibold"><h2 className="hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out">WEB APP DEVELOPMENT</h2><p className="text-base ">Web applicaion have been changing the way of business operates across the globe</p></div>
-                            <div className="text-[#ffffff] text-3xl font-semibold"><h2 className="hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out">MOBILE APP DEVELOPMENT</h2><p className="text-base ">on this modren era, business should not ignore to have a business mobile app.</p></div></div>
+                        <div className="text-right">
+
+                            <div className=" py-8 text-[#ffffff] text-3xl">
+                                <h2 className="font-semibold hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out">WEBSITE DEVELOPMENT</h2>
+                                <p className="text-base">Developing a website is fun and anyone can do this but very few understand how to build websites</p>
+                            </div>
+
+                            <div className=" py-8 text-[#ffffff] text-3xl">
+                                <h2 className="font-semibold hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out">WEB APP DEVELOPMENT</h2>
+                                <p className="text-base ">Web applicaion have been changing the way of business operates across the globe</p>
+                            </div>
+
+                            <div className=" py-8 text-[#ffffff] text-3xl">
+                                <h2 className="font-semibold hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out">MOBILE APP DEVELOPMENT</h2>
+                                <p className="text-base ">on this modren era, business should not ignore to have a business mobile app.</p>
+                            </div>
+
+                        </div>
+
+
                         <div></div>
 
+
                     </div>
-                    <div className="w-[20%] py-8"><img src={iphonePic} width="100%" height="100%" /></div>
+
+                    <div className="w-[20%] py-14">
+                        <img src={iphonePic} width="100%" height="100%" />
+                    </div>
+
                     <div className="w-[20%]">
 
 
-                        <div className="text-left"><div className="text-[#ffffff] text-3xl font-semibold"><h2 className="hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out">CLOUD SERVICES</h2><p className="text-base">Modernize your workflow and improve your application's performance by migrating to the cloud</p></div>
-                            <div className="text-[#ffffff] text-3xl font-semibold"><h2 className="hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out">DIGITAL MARKETING</h2><p className="text-base">Are you looking for a ways to make your pitch inspirational to social media audience?</p></div>
-                            <div className="text-[#ffffff] text-3xl font-semibold"><h2 className="hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out">STARTUP SOLUTIONS</h2><p className="text-base">We help enterpreneurs turn their vision into reality and guide them throughout thier business life cycle.</p></div></div>
+                        <div className="text-left">
+                            <div className="py-8 text-[#ffffff] text-3xl ">
+                                <h2 className="hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out font-semibold">CLOUD SERVICES</h2>
+                                <p className="text-base">Modernize your workflow and improve your application's performance by migrating to the cloud</p>
+                            </div>
+
+                            <div className="py-8 text-[#ffffff] text-3xl ">
+                                <h2 className="font-semibold hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out">DIGITAL MARKETING</h2>
+                                <p className="text-base">Are you looking for a ways to make your pitch inspirational to social media audience?</p>
+                            </div>
+
+                            <div className="py-8 text-[#ffffff] text-3xl ">
+                                <h2 className="font-semibold hover:text-[#fb4630] cursor-pointer transition delay-150 duration-300 ease-in-out">STARTUP SOLUTIONS</h2>
+                                <p className="text-base">We help enterpreneurs turn their vision into reality and guide them throughout thier business life cycle.</p>
+                            </div>
+                        </div>
+
+
                         <div></div>
                     </div>
                 </div>
