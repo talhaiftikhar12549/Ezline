5573a973594ed8b7a99c637d3ae72dee27b8a621
talhaiftikhar12549 <talhaiftikhar12549@gmail.com>
2024-12-03 00:48:25 +0500

    main nav bar compeleted

--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -1,13 +1,16 @@
 import pashaImages from "../assets/Pasha.png"
 import rightAngleTriangle from "../assets/right-triangle.png"
 import googleReview from "../assets/google-review.png"
+import caenderLogo from "../assets/calender logo.svg"
+import sudiaLogo from "../assets/ezlinefooterlogo.svg"
+import CMLogo from "../assets/ezlinefooterlogo2.svg"
 export default function MainFooter() {
     return (
         <>
-            <div className="h-[95vh] bg-[#212331]">
+            <div className="h-[100vh] bg-[#212331]">
 
 
-                <div className="flex px-40 py-40 w-[100%]">
+                <div className="flex px-40 py-32 w-[100%]">
 
 
                     <div className="w-[33%] px-3">
@@ -24,35 +27,32 @@ export default function MainFooter() {
                     <div className="w-[33%] px-3">
                         <h2 className="text-[#fcfeff] py-8 text-xl font-bold">RECENT POST &nbsp;<img className="inline h-2 w-2" src={rightAngleTriangle} alt="pasha" /></h2>
                         <div>
-                            <div className="w-[100%] flex border-b py-4">
+                            <div className="w-[100%] flex border-b-[1px] py-4">
                                 <div className="w-[30%]">
-                                    <div className="rounded-full border p-4"></div>
-                                    <img src="" alt="" />
+                                    <img src={caenderLogo} alt="" className="w-20 h-20" />
                                 </div>
                                 <div className="w-[70%]">
-                                    <p className="text-[#829aae] ">16th Anniversary</p>
+                                    <p className="text-[#829aae] hover:text-[#02b8cd]">16th Anniversary</p>
                                     <p className="text-[#02b8cd]">Febrarury 4, 2024</p>
                                 </div>
                             </div>
 
-                            <div className="w-[100%] flex border-b border-[1px] py-4">
+                            <div className="w-[100%] flex border-b-[1px] py-4">
                                 <div className="w-[30%]">
-                                    <div className="rounded-full border p-4"></div>
-                                    <img src="" alt="" />
+                                <img src={sudiaLogo} alt="" className="w-20 h-20"/>
                                 </div>
                                 <div className="w-[70%]">
-                                    <p className="text-[#829aae] ">presendental award won by ezline Software House in IT sector</p>
+                                    <p className="text-[#829aae] hover:text-[#02b8cd]">presendental award won by ezline Software House in IT sector</p>
                                     <p className="text-[#02b8cd]">Febrarury 4, 2024</p>
                                 </div>
                             </div>
 
                             <div className="w-[100%] flex  py-4">
                                 <div className="w-[30%]">
-                                    <div className="rounded-full border p-4"></div>
-                                    <img src="" alt="" />
+                                <img src={CMLogo} alt="" className="w-20 h-20"/>
                                 </div>
                                 <div className="w-[70%]">
-                                    <p className="text-[#829aae] ">Ezline Exibition at LEAP 2023 Sudia Arabia</p>
+                                    <p className="text-[#829aae] hover:text-[#02b8cd]">Ezline Exibition at LEAP 2023 Sudia Arabia</p>
                                     <p className="text-[#02b8cd]">Febrarury 4, 2024</p>
                                 </div>
                             </div>
